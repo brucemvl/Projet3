f6df5fc9d5347022c76755fb0358b7a982db82d4
bruce <bruce.monnerville@hotmail.fr>
2024-06-11 00:03:33 +0200

    avancement login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,28 +1,46 @@
 let mail = document.getElementById("email")
 let motDePasse = document.getElementById("password")
-let mailUtilisateur = mail.value
-let motDePasseUtilisateur = motDePasse.value
-let valider = document.getElementById("valider")
-let formulaireConnexion = document.getElementById("formulaireConnexion")
+let msgErreurMail = document.querySelector(".emailerreur")
+let msgErreurMdp = document.querySelector(".mdperreur")
+
+let valider = document.querySelector(".valider")
 
 let apiLoginUrl = "http://localhost:5678/api/users/login"
 
-/*fetch(apiLoginUrl, {
+fetch(apiLoginUrl, {
     method: "POST",
-    body: `"{"${mailUtilisateur}": "${motDePasseUtilisateur}"}"`,
+    body: `"{"${mail.value}": "${motDePasse.value}"}"`,
     headers: "accept: application.json"
-})*/
+})
+
+function erreur(champ, msg){
+    if(champ.value.length < 1){
+    let msgErreur = document.createElement("p")
+    msg.appendChild(msgErreur)
+    
+    msgErreur.innerText = "Veuillez remplir ce champ"
+    }
+}
+
+function validerMail(adressemail) {
+    let regex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+    let resultat = regex.test(adressemail)
+    
+    if (resultat === true){
+    return true}
+    else {
+        msgErreurMail.innerHTML = "email non valide"
+    }
+    }
 
 
 
-    valider.addEventListener("click", (event)=>{
-        
-        if(mailUtilisateur.length < 1 || motDePasseUtilisateur.length < 1){
-            event.preventDefault();
-            console.log("vide")
+valider.addEventListener("click", ()=>{
+    
+    erreur(mail, msgErreurMail)
+    msgErreurMdp.innerHTML = ""
+    erreur(motDePasse, msgErreurMdp)
 
-        }
-        else{}
-    })
+    validerMail(mail.value)
 
-
+})
